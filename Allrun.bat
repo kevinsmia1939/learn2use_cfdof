2f23eabed7b0b2cbb532e3c0c726e55870c0bd48
Kavin Teenakul <andythe_great@protonmail.com>
2025-11-02 23:21:06 +0100

    first

---
+++ Allrun.bat
@@ -0,0 +1,29 @@
+@echo off
+
+REM Source runtime environment
+
+set FOAMDIR="/usr/lib/openfoam/openfoam2506"
+set CWD=%CD%
+
+if None GEQ 1000 goto OPENCFD
+:FOUNDATION
+set OLDPATH=%PATH%
+call %FOAMDIR%\setvars_OFNone.bat
+set PATH=%PATH%;%OLDPATH%
+
+REM Fix for error in FOAM_MPI in setvars-OF.bat
+FOR /F "tokens=* USEBACKQ" %%F IN (`dir /b %%FOAM_LIBBIN%%\MS-MPI*`) DO (
+SET FOAM_MPI=%%F
+)
+set PATH=%FOAM_LIBBIN%\%FOAM_MPI%;%PATH%
+goto CONTINUE
+
+:OPENCFD
+call %FOAMDIR%\setEnvVariables-vNone.bat
+
+:CONTINUE
+
+cd /d %CWD%
+
+REM Run PowerShell script
+PowerShell -NoProfile -ExecutionPolicy Bypass -File Allrun.ps1
